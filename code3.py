141ee4dda72df7214d80609e031df579af0922f5
Anushka Mittal <anushkamittal0@gmail.com>
2019-08-21 20:33:22 +0530

    Added code3

---
+++ code3.py
@@ -0,0 +1,6 @@
+celsius=float(input("Enter temperature in celsius="))
+farenhite=(celsius*9/5)+32
+print("Temperatue in farenhite is=",farenhite)
+farenhite=float(input("Enter temperature in farehnite="))
+celsius=(farenhite-32)*5/9
+print("Temperature in celsius is=",celsius)
