9ed79cb99952b3dc05aa8019b88d66ba9f4ecb62
Anushka Mittal <anushkamittal0@gmail.com>
2019-08-28 18:42:22 +0530

    Added code21

---
+++ code21.py
@@ -0,0 +1,10 @@
+eng=float(input("Enter marks for english="))
+hindi=float(input("Enter marks for hindi="))
+maths=float(input("Enter marks for mathematics="))
+sci=float(input("Enter marks for science="))
+sst=float(input("Enter marks for social studies="))
+tot=eng+hindi+maths+sci+sst
+print("Total marks are=",tot)
+avg=tot/5
+print("Average =",avg)
+
