e9fb6d0ce7a6f1f54cf079bcf8e4380966803f35
Anushka Mittal <anushkamittal0@gmail.com>
2019-08-28 18:53:30 +0530

    Added code23

---
+++ code23.py
@@ -0,0 +1,6 @@
+w=float(input("Enter value for w="))
+x=float(input("Enter value for x="))
+y=float(input("Enter value for y="))
+z=float(input("Enter value for z="))
+p=w+x/y-z
+print("Value for p is=",p)
