5bbc1c4dd2db5ee94d90e864c79af4d1234c96e7
Anushka Mittal <anushkamittal0@gmail.com>
2019-08-30 20:58:14 +0530

    Added code31

--- code31.py
+++ code31.py
@@ -1,6 +1,2 @@
-a=[]
-n=int(input("Enter number of elements="))
-for i in range(0,n):
-	element=int(input())
-	a.append(element)
-print(a)
+a=input("Enter your string=")
+print("Length of your string is=",len(a))
