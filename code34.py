ca19c1b6767f13465ec25c7a042207babf2a712f
Anushka Mittal <anushkamittal0@gmail.com>
2019-09-01 16:39:35 +0530

    Added code34

--- code34.py
+++ code34.py
@@ -1,13 +1,21 @@
 d=[]
 a=int(input("Enter the numbers for array="))
-for i in range(0,a):
+while True:
+	if len(d) == a:
+		break
+
 	ele=int(input())
+	if ele in d:
+		print("Number already in array")
+		continue
+
 	d.append(ele)
+
 print("Array is:",d)
 search=int(input("Enter the number you want to search in array="))
 if search in d:
 	print("Number searched:",search)
-else:
+else:	
 	print("Number not found")
 
 
